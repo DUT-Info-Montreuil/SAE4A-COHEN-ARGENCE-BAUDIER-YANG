6dbb57450a06c54b0ddf06e4b200e34fae8cfc5c
ismael argence <ismaelargence3@gmail.com>
2023-04-05 11:36:37 +0200

    modification du form pour matcher la bd

--- app/src/main/java/com/example/vraiburgir/SignupActivity.java
+++ app/src/main/java/com/example/vraiburgir/SignupActivity.java
@@ -11,10 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private EditText editTextPseudo;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextConfirmPassword;
     private EditText editTextAddress;
+    private EditText editPhoneNumber;
     private EditText editTextFirstName;
     private EditText editTextLastName;
 
@@ -23,10 +25,12 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        editTextPseudo = findViewById(R.id.editTextPseudo);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
         editTextAddress = findViewById(R.id.editTextAddress);
+        editPhoneNumber = findViewById(R.id.editPhoneNumber);
         editTextFirstName = findViewById(R.id.editTextFirstName);
         editTextLastName = findViewById(R.id.editTextLastName);
 
@@ -35,15 +39,19 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Récupère les valeurs saisies par l'utilisateur
+                String pseudo = editTextPseudo.getText().toString().trim();
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
                 String confirmPassword = editTextConfirmPassword.getText().toString().trim();
                 String address = editTextAddress.getText().toString().trim();
+                String phone = editPhoneNumber.getText().toString().trim();
                 String firstName = editTextFirstName.getText().toString().trim();
                 String lastName = editTextLastName.getText().toString().trim();
 
                 // Vérifie si les champs ne sont pas vides
-                if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || address.isEmpty() || firstName.isEmpty() || lastName.isEmpty()) {
+                if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() ||
+                        address.isEmpty() || firstName.isEmpty() || lastName.isEmpty() ||
+                        pseudo.isEmpty() || phone.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -57,6 +65,13 @@ public class SignupActivity extends AppCompatActivity {
                 // Vérifie si les mots de passe correspondent
                 if (!password.equals(confirmPassword)) {
                     Toast.makeText(getApplicationContext(), "Les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Vérifie si le numéro de telephone est valide
+                String phonePattern = "^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$";
+                if (!phone.matches(phonePattern)){
+                    Toast.makeText(getApplicationContext(), "Le numéro de télephone n'est pas valide", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
